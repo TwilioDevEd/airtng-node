968ff78608b0af37908def8defbb291087319651
Agustin Camino <agustin.camino@gmail.com>
2015-12-21 11:23:13 -0500

    Refactor reservations to improve readability

--- routes/reservations.js
+++ routes/reservations.js
@@ -3,23 +3,23 @@ var router = express.Router();
 var Property = require('../models/property');
 var Reservation = require('../models/reservation');
 
-router
-  .post('/', function (req, res) {
-    var propertyId = req.body.propertyId;
+// POST: /reservations
+router.post('/', function (req, res) {
+  var propertyId = req.body.propertyId;
 
-    Property.findOne({ _id: propertyId })
-    .then(function (property) {
-      var reservation = new Reservation({
-        name: req.body.name,
-        message: req.body.message,
-        status: 'pending',
-        property: propertyId
-      });
-
-      return reservation.save();
-    }).then(function (savedReservation) {
-      res.redirect('/properties');
+  Property.findOne({ _id: propertyId })
+  .then(function (property) {
+    var reservation = new Reservation({
+      name: req.body.name,
+      message: req.body.message,
+      status: 'pending',
+      property: propertyId
     });
+
+    return reservation.save();
+  }).then(function (savedReservation) {
+    res.redirect('/properties');
   });
+});
 
 module.exports = router;
