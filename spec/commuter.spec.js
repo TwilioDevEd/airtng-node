a633abd434bf276156bc84fcf71d188b147d239f
Agustin Camino <agustin.camino@gmail.com>
2015-12-29 17:23:42 -0500

    Add test for commuter route

---
+++ spec/commuter.spec.js
@@ -0,0 +1,77 @@
+require('./connectionHelper');
+var fixtures = require('./fixtures');
+
+var expect = require('chai').expect;
+var supertest = require('supertest');
+var cheerio = require('cheerio');
+
+var app = require('../app.js');
+var User = require('../models/user');
+var Reservation = require('../models/reservation');
+var Property = require('../models/property');
+var agent = supertest(app);
+
+describe('commuter', function () {
+  describe('POST /commuter/use-sms', function () {
+    beforeEach(function (done) {
+      new User(fixtures.host).save()
+      .then(function (owner) {
+        return new Property({
+          description: 'Papallacta',
+          imageUrl: 'http://image.info',
+          owner: owner.id
+        }).save();
+      })
+      .then(function (property) {
+        var propertyPromise = Promise.resolve(property);
+        var guestPromise =  new User(fixtures.guest).save();
+
+        return Promise.all([propertyPromise, guestPromise]);
+      })
+      .then(function (data) {
+        property = data[0];
+        guest = data[1];
+        new Reservation({
+          message: 'reservation',
+          property: property.id,
+          guest: guest.id,
+          phoneNumber: '+555 0000'
+        }).save(done);
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+    });
+
+    afterEach(function (done) {
+      Reservation.remove({})
+      .then(function () {
+        Property.remove({});
+      })
+      .then(function () {
+        User.remove({}, done);
+      });
+    });
+
+    context('when the caller is the guest', function () {
+      it('connects the guest to the host using SMS', function (done) {
+        // Create a user for guest
+        agent.post('/commuter/use-sms')
+        .send({
+          From: '555 5556',
+          To:   '555 0000',
+          Body: 'awesome message'
+        })
+        .expect(200)
+        .end(function (err, res) {
+          if (err) {
+            return done(err);
+          }
+          var $ = cheerio.load(res.text);
+          expect($('Message').attr('to')).to.equal('555 5555');
+          done();
+        });
+      })
+    });
+  });
+});
---
+++ spec/fixtures.js
@@ -0,0 +1,20 @@
+module.exports = {
+  host: {
+    username: 'bob',
+    email: 'bob@example.com',
+    password: 's3cr3t',
+    countryCode: '+1',
+    areaCode: '541',
+    phoneNumber: '555 5555'
+  },
+
+  guest: {
+    username: 'alice',
+    email: 'alice@example.com',
+    password: 's3cr3t',
+    countryCode: '+1',
+    areaCode: '541',
+    phoneNumber: '555 5556'
+  },
+
+}
